cabf135e6c1279ba0c0204d24c1fb90e4c57e55f
lakhendrakushwah <73408930+lakhendrakushwah@users.noreply.github.com>
2020-12-24 01:21:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,28 +27,12 @@ function sortTasks(){
 	$('#ulTasks .done').appendTo(ulTasks)
 }
 
-function toggleInputBtn(valIsEmpty){
-	if(!valIsEmpty){
-		btnReset.prop('disabled',false)
-		btnAdd.prop('disabled',false)
-	}
-	else{
-		btnReset.prop('disabled',true)
-		btnAdd.prop('disabled',true)	
-	}
-}
-
 inpNewTask.keypress( function(e){
 	if (e.which == 13 && inpNewTask.val() != '' ) addItem()
 })
-inpNewTask.oninput(function(){
-	toggleInputBtn(inpNewTask.val() != '')
-})
-
 btnAdd.click(addItem)
 btnReset.click(function(){
  inpNewTask.val('')
- toggleInputBtn(false)
 })
 btnCleanup.click(clearDone)
 btnSort.click(sortTasks)
