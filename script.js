f5b7aeb88562761da215062b9ed53bbf5987a655
lakhendrakushwah <73408930+lakhendrakushwah@users.noreply.github.com>
2020-12-24 15:05:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ function addItem() {
 	ulTasks.append(listItem)
 	inpNewTask.val('')
 
+	toggleInputBtn(inpNewTask.val())
+
 }
 
 
@@ -27,12 +29,50 @@ function sortTasks(){
 	$('#ulTasks .done').appendTo(ulTasks)
 }
 
+
+
+function toggleInputBtn(valIsEmpty){
+	if(valIsEmpty){
+		btnReset.prop('disabled',false)
+		btnAdd.prop('disabled',false)
+	}
+	else{
+		btnReset.prop('disabled',true)
+		btnAdd.prop('disabled',true)	
+	}
+}
+
 inpNewTask.keypress( function(e){
 	if (e.which == 13 && inpNewTask.val() != '' ) addItem()
 })
+
+
+inpNewTask.on('input', function(){
+	console.log(inpNewTask.val())
+	toggleInputBtn(inpNewTask.val())
+})
+/*
+inpNewTask.on('input', function(){
+	console.log(inpNewTask.val())
+	
+	if(inpNewTask.val() != ''){
+		btnReset.prop('disabled',false)
+		btnAdd.prop('disabled',false)
+	}
+	else{
+		btnReset.prop('disabled',true)
+		btnAdd.prop('disabled',true)
+	}
+})
+*/
+
+
 btnAdd.click(addItem)
+
+
 btnReset.click(function(){
  inpNewTask.val('')
+ btnReset.prop('disabled',true)
 })
 btnCleanup.click(clearDone)
 btnSort.click(sortTasks)
